c04c5a51cb52cf9d93a1e58bdeb4385afbeb022e
Scott S. McCoy <smccoy@saymedia.com>
2012-11-19 13:51:42 -0800

    AnyEvent::Memcached::Hash::Consistent.
     * Created a consistent hashing implementation for ``AnyEvent::Memcached``
       based on ``Set::Consistent``.
     * Added build script.

---
+++ lib/AnyEvent/Memcached/Hash/Consistent.pm
@@ -0,0 +1,87 @@
+package AnyEvent::Memcached::Hash::Consistent;
+
+=head1 NAME
+
+AnyEvent::Memcached::Hash::Consistent - Hash using a consistent hashing
+algorithm.
+
+=head1 DESCRIPTION
+
+This module hashes using L<Set::ConsistentHash> as a hashing algorithm.  The
+result is that the distribution of keys is consistently displaced across 1024
+"virtual" buckets, and adding a new memcached server has a network-wide
+cache-miss ratio of n / e until 1024 systems.
+
+=cut
+
+use common::sense;
+use Set::ConsistentHash;
+
+=head1 CONSTRUCTOR
+
+=over 4
+
+=item new LOTS_OF_THINGS.
+
+Accepts a balanced list of attributes.
+
+=over 10
+
+=item buckets
+
+AnyEvent::Memcached::Buckets
+
+=back
+
+=cut
+
+sub new {
+    my ($class) = shift;
+
+    my $self = $class->SUPER::new(@_);
+
+    my $hash = $self->{hash} = Set::ConsistentHash->new;
+
+    # Ignore the bucketer....just use it for a set of peers.
+    my $peers = $self->{buckets}->peers;
+
+    $hash->targets( map +($_ => 1), keys %$peers );
+}
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item servers KEY | [ KEYS... ]
+
+Return a hash of peer keys, each holding a reference to an array of servers to
+map to.
+
+=cut
+
+sub servers {
+    my ($self, $keys) = @_;
+
+    if (ref $keys ne "ARRAY") {
+        $keys = [ $keys ];
+    }
+
+    my $hash   = $self->{hash};
+    my $result = {};
+
+    for my $key (@$keys) {
+        my $peer = $hash->get_target($key);
+
+        push $result->{$peer} //= [], $key;
+    }
+
+    return $result;
+}
+
+=back
+
+=cut
+
+return __PACKAGE__;
